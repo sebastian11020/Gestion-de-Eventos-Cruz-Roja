ef69242bdfdb498746df4b5295c9be46f78649a8
sebas <Sebastiandd87@gmnail.com>
2025-09-04 17:18:51 -0500

    Add components

--- cruzroja-frontend/src/app/page.tsx
+++ cruzroja-frontend/src/app/page.tsx
@@ -62,8 +62,8 @@ export default function Home() {
             aria-hidden
             src="/file.svg"
             alt="File icon"
-            width={17}
-            height={17}
+            width={15}
+            height={15}
           />
           Learn
         </a>
