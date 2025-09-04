25f87359f51b7430e034f349d0858058759189ec
sebas <Sebastiandd87@gmnail.com>
2025-09-04 16:50:29 -0500

    Create project with next and tailwind css and components with shadcn

--- cruzroja-frontend/src/app/page.tsx
+++ cruzroja-frontend/src/app/page.tsx
@@ -62,8 +62,8 @@ export default function Home() {
             aria-hidden
             src="/file.svg"
             alt="File icon"
-            width={16}
-            height={16}
+            width={17}
+            height={17}
           />
           Learn
         </a>
