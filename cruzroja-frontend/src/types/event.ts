a8fcb76f607afffdf0dcb22dae661f73f445412a
sebastian11020 <sebastiandd87@gmail.com>
2025-09-06 17:58:12 -0500

    Add login and structure

---
+++ cruzroja-frontend/src/types/event.ts
@@ -0,0 +1,10 @@
+export type EventoEstado = "Activo" | "Cerrado" | "Borrador"
+
+export interface Evento {
+    id: string
+    titulo: string
+    fechaInicio: Date
+    fechaFin?: Date
+    estado: EventoEstado
+    lugar?: string
+}
