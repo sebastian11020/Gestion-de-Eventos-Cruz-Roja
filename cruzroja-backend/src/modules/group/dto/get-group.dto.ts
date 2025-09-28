3ecc1633c73731058c8f8b6ce5896f0a2fadc37d
Juan Rodriguez <juansebastianrodriguezmateus7b@gmail.com>
2025-09-28 12:33:16 -0500

    Added group-headquarters module

--- cruzroja-backend/src/modules/group/dto/get-group.dto.ts
+++ cruzroja-backend/src/modules/group/dto/get-group.dto.ts
@@ -1,15 +1,4 @@
 export class GetGroupDto {
   id: string;
   name: string;
-  sectional: string;
-  numberVolunteers: string;
-  numberPrograms: string;
-  leader?: {
-    document: string;
-    name: string;
-  };
-  programs?: {
-    id: string;
-    name: string;
-  };
 }
