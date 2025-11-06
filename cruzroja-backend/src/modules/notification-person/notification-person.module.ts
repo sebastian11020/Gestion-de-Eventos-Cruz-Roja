a5f51ac35534e44074a0294a46b2269d56b0fdff
Juan Rodriguez <juansebastianrodriguezmateus7b@gmail.com>
2025-11-06 10:41:50 -0500

    added notifications

--- cruzroja-backend/src/modules/notification-person/notification-person.module.ts
+++ cruzroja-backend/src/modules/notification-person/notification-person.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { NotificationPersonService } from './notification-person.service';
+import { NotificationPersonController } from './notification-person.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotificationPerson } from './entity/notification-person.entity';
+import { SupabaseModule } from '../../common/config/supabase/supabase.module';
+import { SupabaseAuthModule } from '../../common/config/guards/supabase-auth.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([NotificationPerson]),
+    SupabaseModule,
+    SupabaseAuthModule,
+  ],
   providers: [NotificationPersonService],
+  controllers: [NotificationPersonController],
+  exports: [NotificationPersonService],
 })
 export class NotificationPersonModule {}
--- cruzroja-backend/src/modules/notification/notification.module.ts
+++ cruzroja-backend/src/modules/notification/notification.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Notification } from './entity/notification';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Notification])],
   providers: [NotificationService],
   exports: [NotificationService],
 })
